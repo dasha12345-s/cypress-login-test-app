696b7c137bbb44fbb081e982976f6946278a4f4e
Darya Shostak <shostak19@icloud.com>
2025-10-13 17:21:24 -0400

    button and link tests added

--- cypress/e2e/login-ui-ux-checklist.cy.js
+++ cypress/e2e/login-ui-ux-checklist.cy.js
@@ -17,17 +17,48 @@ describe('Login UI/UX Tests', () => {
     })
 
     describe('Username/email and password fields labeled', () => {
-         it('should contain username/email field', () => {
+        it('should contain username/email field', () => {
             cy.contains('Email Address').should('be.visible')
             cy.get('[data-cy="email-input"]')
                 .should('be.visible')
                 .and('have.attr', 'placeholder', 'Enter your email')
         })
-         it('should contain password field', () => {
+        it('should contain password field', () => {
             cy.contains('Password').should('be.visible')
             cy.get('[data-cy="password-input"]')
                 .should('be.visible')
                 .and('have.attr', 'placeholder', 'Enter your password')
-  })
+        })
+    })
+
+    describe('Button visibility and state', () => {
+        it('should display visible and aligned login button', () => {
+            cy.get('[data-cy="login-button"]')
+                .should('be.visible')
+                .and('contain', 'Login')
+                .and('have.attr', 'type', 'submit')
+                .and('have.attr', 'id', 'login-btn')
+        })
+    })
+
+    describe('Links visible and working', () => {
+        it('should display "Forgot password?" link', () => {
+            cy.get('[data-cy="forgot-password"]')
+                .should('be.visible')
+                .and('contain', 'Forgot password?')
+                .click()
+            cy.get('[class="modal-content"]')
+                .should('be.visible')
+            cy.get('[data-cy="close-modal"]')
+                .click()
+            cy.get('[class="modal-content"]')
+                .should('not.be.visible')
+        })
+    })
+        it('should display "Sign up" link with correct href', () => {
+            cy.get('[data-cy="signup-link"]')
+                .should('be.visible')
+                .and('contain', 'Sign up')
+                .and('have.attr', 'href', 'pages/signup.html')
     })
 })
