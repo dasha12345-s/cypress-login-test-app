6d7593b7f83d5a9c4f9a33db40b00b46ea970a57
Darya Shostak <shostak19@icloud.com>
2025-10-29 13:19:05 -0400

    some changes

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,17 @@ module.exports = defineConfig({
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    reportPageTitle: 'Cypress Login Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
   viewportWidth: 1280,
   viewportHeight: 720,
   video: false,
